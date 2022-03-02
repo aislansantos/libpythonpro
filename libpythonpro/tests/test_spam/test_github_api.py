64230de9115c64faf4b85f2bc2a03fe521627ebb
Aislan Santos <aislan.santos@gmail.com>
2022-03-01 22:42:02 -0300

    pytest - Biblioteca Pytest-Mock - Neesse tópico você vai aprender como escrever mocks de bibliotecas usando o plugin pytest-mock.

--- libpythonpro/tests/test_spam/test_github_api.py
+++ libpythonpro/tests/test_spam/test_github_api.py
@@ -1,27 +1,26 @@
 from unittest.mock import Mock
 import pytest
+from pytest_mock import mocker
+
 from libpythonpro import github_api
 
+
 @pytest.fixture
-def avatar_url():
+def avatar_url(mocker):
     resp_mock = Mock()
     url = 'https://avatars.githubusercontent.com/u/53060089?v=4'
     resp_mock.json.return_value = {
         'login': 'aislansantos', 'id': 53060089, 'node_id': 'MDQ6VXNlcjUzMDYwMDg5',
         'avatar_url': url
     }
-    get_original = github_api.requests.get
-    github_api.requests.get = Mock(
-        return_value=resp_mock)  # trocamos aqui a api do github para para um modulo interno isolado
-    yield url
-    github_api.requests.get = get_original  # TEAR DOWN volto o meu get para o valor orginal para isolar esse meu teste do teste de integração
-
+    get_mock = mocker.patch('libpythonpro.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
 
 
 def teste_buscar_avatar(avatar_url):
     url = github_api.buscar_avatar('aislansantos')
     assert avatar_url == url
-
 
 
 def teste_buscar_avatar_integracao():
